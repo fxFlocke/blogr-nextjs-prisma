6468263460778f6197e1816b542678a1d620836a
felix_blockchainer <97702158+flockchain@users.noreply.github.com>
2023-07-12 01:26:12 +0200

    updated note char limit

--- components/Forms/NoteForm.tsx
+++ components/Forms/NoteForm.tsx
@@ -69,7 +69,7 @@ export function NoteForm({noteSubmitAPI} : {noteSubmitAPI: (arg1: string, arg2: 
 
                                                         <div>
                                                             <label form="chart" className="block mb-2 text-sm font-medium text-black dark:text-gray-300">Content</label>
-                                                            <textarea id="trackedPerson" value={content} onInput={(e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)} maxLength={50} className="h-[105px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                                                            <textarea id="trackedPerson" value={content} onInput={(e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)} maxLength={250} className="h-[105px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                                                         </div>
                                                     </div>
                                                 </div>
